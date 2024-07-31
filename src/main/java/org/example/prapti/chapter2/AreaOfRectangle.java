36d5acc835e6fa0474b4b0de0d72859c3b166645
prapti-bhatta <praptibhatta0@gmail.com>
2024-07-31 17:12:24 +0545

    Practice questions

--- src/main/java/org/example/prapti/chapter2/AreaOfRectangle.java
+++ src/main/java/org/example/prapti/chapter2/AreaOfRectangle.java
@@ -2,7 +2,7 @@ package org.example.prapti.chapter2;
 
 public class AreaOfRectangle {
     public static void main(String[] args) {
-        int l=2, w=3 ,Area;
+        int l=2, w=3 ;
         System.out.println("Area : " +(l*w));
     }
 }

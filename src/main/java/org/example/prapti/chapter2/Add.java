b033e405e92182343000da2f39c848c00b210492
prapti-bhatta <praptibhatta0@gmail.com>
2024-07-31 17:11:42 +0545

    Practice questions

--- src/main/java/org/example/prapti/chapter2/Add.java
+++ src/main/java/org/example/prapti/chapter2/Add.java
@@ -2,8 +2,7 @@ package org.example.prapti.chapter2;
 
 public class Add {
     public static void main(String[] args) {
-        int a=2 , b=3 , c;
+        int a=2 , b=3 ;
         System.out.println("c :" +(a + b));
-        c= a+b;
     }
 }

98b9d86f67666aabdf0306f2a6a8fb18e8dd69d4
prapti-bhatta <praptibhatta0@gmail.com>
2024-07-31 17:13:34 +0545

    Practice questions

--- src/main/java/org/example/prapti/chapter2/EvenOdd.java
+++ src/main/java/org/example/prapti/chapter2/EvenOdd.java
@@ -3,7 +3,6 @@ package org.example.prapti.chapter2;
 public class EvenOdd {
     public static void main(String[] args) {
         int a=2;
-        System.out.println("a :" + a );
         if (a % 2 == 0) {
             System.out.println("a is even");
         } else {
